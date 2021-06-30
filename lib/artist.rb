aa1e40f84bf7380b0210cddda0a35bd1c15a36e7
Kaia <kbergmanndean@gmail.com>
2021-06-30 16:00:05 -0400

    commit tree 7e72a1a1b36224a8d91364b5da3f5b8b9f1ee7cf

---
+++ lib/artist.rb
@@ -0,0 +1,14 @@
+class Artist
+    attr_accessor :name
+    def initialize(name)
+        @name=name
+    end
+    def add_song_by_name(name)
+        song=Song.new(name)
+        song.artist=self
+    end
+    def songs
+        song.all.select{|song|song.artist==self}
+    end
+end
+
